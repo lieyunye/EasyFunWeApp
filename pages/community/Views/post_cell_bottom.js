b85387694da840cc5d313d71810a92cf72798fdd
lyy <lieyunye@163.com>
2018-04-01 21:06:56 +0800

    增加post_cell_bottom

--- pages/community/Views/post_cell_bottom.js
+++ pages/community/Views/post_cell_bottom.js
@@ -6,7 +6,34 @@ Component({
     post: {
       type: Object,
       value: {},
+      observer: function (newVal, oldVal) {
+        if (newVal.noteDynamic)
+        {
+          console.log('list observer ready ' + newVal.noteDynamic.replyCount)
+          console.log('list observer ready ' + newVal.noteDynamic.praiseCount)
+          console.log('list observer ready ' + newVal.noteDynamic.treadCount)
+          var upDownCount = (newVal.noteDynamic.praiseCount - newVal.noteDynamic.treadCount)
+          this.setData({
+            comment: { 'name': '/images/ic_community_blackrevert.png', 'class': 'share', 'text': newVal.noteDynamic.replyCount > 0 ? newVal.noteDynamic.replyCount + '':'' },
+            upDownCount: upDownCount == 0 ? '' : upDownCount + ''
+
+          })
+        }
+        }
     },
+    share:{
+      type:Object,
+      value: { 'name':'/images/ic_community_blackshare.png','class':'share'}
+    },
+    comment: {
+      type: Object,
+      value: {}
+    },
+    upDownCount: {
+      type: String,
+      value: 0
+    }
+
   }
 
 })
